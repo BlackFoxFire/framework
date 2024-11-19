711e22e5de99c277fa9bc7d109cf09388607174e
Christophe Dufour <blackfox@proximus.be>
2024-11-19 15:35:39 +0100

    Modification de DBFactory

--- src/Blackfox/Factories/DBFactory.php
+++ src/Blackfox/Factories/DBFactory.php
@@ -14,23 +14,67 @@ use Blackfox\Factories\Enums\DatabaseAPI;
 class DBFactory
 {
     /**
-     * Retourne l'instance de la base de données en fonction de l'api utilisée
+     * Propriétes
+     */
+
+    // API utilisée pour l'accès à la base de données
+    protected DatabaseAPI $api;
+    // Nom de la base de données
+    protected string $dbname;
+    // Nom d'utilisateur pour la connexion à la base de données
+    protected string $username;
+    // Mot de passe de l'utilisateur
+    protected string $password;
+    // Instance de la base de données
+    protected mixed $dbInstance = null;
+
+    /**
+     * Constructeur
      * 
-     * @param DatabaseAPIEnum $api
-     * L'api utilisé pour accéder à la base de données
+     * @param DatabaseAPI $api
+     * API utilisée pour l'accès à la base de données
      * @param string $dbname
 	 * Nom de la base de données
 	 * @param string $username
 	 * Nom d'utilisateur pour la connexion à la base de données
 	 * @param string $password
      * Mot de passe de l'utilisateur
-     * @return \PDO
-     * Retourne une instance de PDO
      */
-    public function createDBConnection(DatabaseAPI $api, string $dbname, string $username, string $password): \PDO
+    public function __construct(DatabaseAPI $api,string $dbname, string $username, string $password)
     {
-        $factoryName = "Blackfox\\Database\\" . $api->value . "Factory";
-        return $factoryName::getInstance($dbname, $username, $password);
+        $this->api = $api;
+        $this->dbname = $dbname;
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    /**
+     * Méthodes
+     */
+
+    /**
+     * Retourne l'instance de la base de données en fonction de l'api utilisée
+     * 
+     * @return mixed
+     * Retourne une instance de base de données ou null si une connexion est impossible
+     */
+    public function get(): mixed
+    {
+        if(is_null($this->dbInstance)) {
+            if(empty($this->dbname) || empty($this->username)) {
+                return null;
+            }
+            
+            $factoryName = "Blackfox\\Database\\" . $this->api->value . "Factory";
+
+            if(!class_exists($factoryName)) {
+                throw new \Exception();
+            }
+            
+            $this->dbInstance = $factoryName::getInstance($this->dbname, $this->username, $this->password);
+        }
+        
+        return $this->dbInstance;
     }
 
 }
